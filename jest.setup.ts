ac299b5c7ac81ce19cb3d6a570b0bbf68071dc02
Joel Shinness <me@joelshinness.com>
2026-05-08 13:12:24 -0700

    feat(19-01): extend jest with jest-axe matcher
    - Import toHaveNoViolations from jest-axe
    - Extend expect globally for accessibility testing

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,5 @@
 import '@testing-library/jest-dom'
+import { toHaveNoViolations } from 'jest-axe'
+
+// Extend Jest expect with jest-axe matchers for accessibility testing
+expect.extend(toHaveNoViolations)
